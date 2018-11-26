be47d4970051b24b0c389ba102f52c3de4d7df4e
William Wall <william@williamwall.ie>
2018-11-26 13:21:15 +0000

    git ignore changes

--- src/components/User/Profile.vue
+++ src/components/User/Profile.vue
@@ -1,13 +1,31 @@
 <template>
   <div id="app">
     <!--<h1>{{userName}}</h1>-->
+    <div>
+      <button @click="checkout">Sell black matter gun</button>
+    </div>
   </div>
+
 </template>
 <script>
-  // import firebase from 'firebase'
-  // export default {
-  //   created() {
-  //     var userName = firebase.auth().currentUser;
-  //   }
-  // }
+  export default {
+    methods: {
+      checkout () {
+// this.$checkout.close()
+// is also available.
+        this.$checkout.open({
+          image: 'https://i.imgur.com/1PHlmFF.jpg',
+          locale: 'en',
+          currency: 'BZD',
+          name: 'Blips and Chitz!',
+          description: 'An entire afternoon at Blips and Chitz!',
+          amount: 9999999,
+          panelLabel: 'Play Roy for {{amount}}',
+          token: (token) => {
+// handle the token
+          }
+        })
+      }
+    }
+  }
 </script>
