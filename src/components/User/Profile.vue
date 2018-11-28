23ef7528ddd00fdad42d56ffdd5cc51a6a7b642a
William Wall <william@williamwall.ie>
2018-11-28 09:20:23 +0000

    user profile changes

--- src/components/User/Profile.vue
+++ src/components/User/Profile.vue
@@ -1,31 +1,7 @@
 <template>
   <div id="app">
     <!--<h1>{{userName}}</h1>-->
-    <div>
-      <button @click="checkout">Sell black matter gun</button>
     </div>
-  </div>
-
 </template>
 <script>
-  export default {
-    methods: {
-      checkout () {
-// this.$checkout.close()
-// is also available.
-        this.$checkout.open({
-          image: 'https://i.imgur.com/1PHlmFF.jpg',
-          locale: 'en',
-          currency: 'BZD',
-          name: 'Blips and Chitz!',
-          description: 'An entire afternoon at Blips and Chitz!',
-          amount: 9999999,
-          panelLabel: 'Play Roy for {{amount}}',
-          token: (token) => {
-// handle the token
-          }
-        })
-      }
-    }
-  }
 </script>
