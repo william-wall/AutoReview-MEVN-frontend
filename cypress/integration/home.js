b11b99112e93da3531b1431504ed3b1d678103b9
William Wall <william@williamwall.ie>
2018-12-05 13:44:32 +0000

    test test

--- cypress/integration/home.js
+++ cypress/integration/home.js
@@ -1,8 +1,11 @@
-describe('Home', () => {
+describe("Home", () => {
   beforeEach(() => {
-    cy.visit('/');
+    cy.visit("/");
   });
   it("Shows the AutoReview trademark", () => {
     // cy.get("h1").should('contain','Welcome to © AutoReview 2018');
+  });
+  it("AutoReview logo", () => {
+    cy.get(".router-link-exact-active").should('contain','AutoReview');
   });
 });
