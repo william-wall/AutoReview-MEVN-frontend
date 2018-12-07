d39b3960b322905a92839b13688eab9637e153d2
William Wall <william@williamwall.ie>
2018-12-07 11:32:55 +0000

    tests for unauthenticated feature button route navigation verification

--- cypress/integration/home.js
+++ cypress/integration/home.js
@@ -31,6 +31,18 @@ describe("Home Page", () => {
     it("Shows the Sign in button", () => {
       cy.get("[href=\"/signin\"] > .btn__content").should('contain','Sign in');
     });
+
+    describe("Buttons - Unauthenticated", () => {
+      it("Will redirect to Sign in when user is not authenticated for Explore Gallery", () => {
+        cy.get('.text-sm-right > .info').click();
+        cy.url().should('include','/signin');
+      });
+      it("Will redirect to Sign in when user is not authenticated for Submit Car", () => {
+        cy.get('.text-sm-left > .info').click();
+        cy.url().should('include','/signin');
+      });
+    });
+
   });
 
   describe("Navigation Bar - Route Navigation", () => {
@@ -47,6 +59,10 @@ describe("Home Page", () => {
       cy.url().should('include','/signin');
       cy.get(':nth-child(2) > .btn > .btn__content').click();
       cy.url().should('include','/reset');
+    });
+    it("Will navigate to Home page when the AutoReview logo is clicked", () => {
+      cy.get('.router-link-exact-active').click();
+      cy.url().should('include','/');
     });
   });
 
