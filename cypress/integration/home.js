23c15ab6953c1352370d43d11b6a22e8dd25b49a
William Wall <william@williamwall.ie>
2018-12-07 11:05:39 +0000

    testing for homescreen static elements

--- cypress/integration/home.js
+++ cypress/integration/home.js
@@ -1,11 +1,17 @@
-describe("Home", () => {
+describe("Home Page", () => {
   beforeEach(() => {
     cy.visit("/");
   });
   it("Shows the AutoReview trademark", () => {
     cy.get("h1").should('contain','Welcome to © AutoReview 2k18');
   });
-  it("AutoReview logo", () => {
+  it("Shows AutoReview logo", () => {
     cy.get(".router-link-exact-active").should('contain','AutoReview');
+  });
+  it("Shows Explore Gallery button", () => {
+    cy.get(".text-sm-right > .info > .btn__content").should('contain','Explore Gallery');
+  });
+  it("Shows Submit Car button", () => {
+    cy.get(".text-sm-left > .info > .btn__content").should('contain','Submit Car');
   });
 });
