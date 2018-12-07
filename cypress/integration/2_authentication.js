62569918a5f85fe5dcee5fc1090c6691255273a9
William Wall <william@williamwall.ie>
2018-12-07 13:04:55 +0000

    test to send the user a reset password link

--- cypress/integration/2_authentication.js
+++ cypress/integration/2_authentication.js
@@ -7,7 +7,7 @@ describe("Authentication", () => {
     it("Should Sign up the user given an email, password and re-enter password", () => {
       cy.get('[href="/signup"] > .btn__content').click();
       cy.url().should('include', '/signup');
-      cy.get('#email').type('cypresstesting@gmail.com');
+      cy.get('#email').type('williamwalldeveloper@gmail.com');
       cy.get('#password').type('testpass');
       cy.get('#confirmPassword').type('testpass');
       cy.get('.flex > .btn > .btn__content').click();
@@ -19,9 +19,21 @@ describe("Authentication", () => {
     it("Should login the user given an email and password", () => {
       cy.get('[href="/signin"] > .btn__content').click();
       cy.url().should('include', '/signin');
-      cy.get('#email').type('cypresstesting@gmail.com');
+      cy.get('#email').type('williamwalldeveloper@gmail.com');
       cy.get('#password').type('testpass');
       cy.get(':nth-child(3) > :nth-child(1) > .btn > .btn__content').click();
+      cy.url().should('include', '/');
+    });
+  });
+
+  describe("Reset password", () => {
+    it("Should send the user an email to reset their password", () => {
+      cy.get('[href="/signin"] > .btn__content').click();
+      cy.url().should('include', '/signin');
+      cy.get(':nth-child(2) > .btn > .btn__content').click();
+      cy.url().should('include', '/reset');
+      cy.get('#email').type('williamwalldeveloper@gmail.com');
+      cy.get('.flex > .btn > .btn__content').click();
       cy.url().should('include', '/');
     });
   });
