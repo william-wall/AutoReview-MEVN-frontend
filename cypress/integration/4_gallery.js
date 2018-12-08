d4b498fc05a18812c7509ca9a5d75a085560f9a7
William Wall <william@williamwall.ie>
2018-12-08 12:53:45 +0000

    test to verify that vehicle got added to gallery listings

--- cypress/integration/4_gallery.js
+++ cypress/integration/4_gallery.js
@@ -1,7 +1,6 @@
 describe("Gallery", () => {
 
   describe("Submit Vehicle to Gallery", () => {
-
     it("Should add a vehicle to the gallery with image and other attributes", () => {
       cy.get('.toolbar__items > [href="/gallery/new"] > .btn__content').click();
       cy.url().should('include', '/gallery/new');
@@ -13,7 +12,16 @@ describe("Gallery", () => {
       cy.get('.primary > .btn__content').click();
       cy.visit("/");
     });
-
+    describe("Verify Vehicle is in Gallery List", () => {
+      it("Should verify that submitted vehicle got added to gallery listings", () => {
+        cy.get('.toolbar__items > [href="/gallerys"] > .btn__content').click();
+        cy.get(':nth-child(7) > .xs12 > .card > .container > .layout > .xs7 > .card__actions > .btn > .btn__content').click();
+        cy.get('.primary--text').should('contain', 'Audi Quattro B2');
+        cy.get('.card__text > :nth-child(2)').should('contain', 'The Audi Quattro is a road and rally car, produced by the German automobile manufacturer Audi, part of the Volkswagen Group. It was first shown at the 1980 Geneva Motor Show on 3 March.[1][3] Production continued through 1991.');
+        cy.visit("/");
+      });
+    });
   });
+
 });
 
