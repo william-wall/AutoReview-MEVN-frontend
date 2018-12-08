dac50dc1369a6132e4536beb746a4a48d30dbf90
William Wall <william@williamwall.ie>
2018-12-08 13:38:14 +0000

    test to add a new chat room to the room list

---
+++ cypress/integration/5_chat_room.js
@@ -0,0 +1,23 @@
+describe("Chat Rooms", () => {
+
+  describe("Add a Chat Room to list", () => {
+    it("Should add a Chat Room to the list of rooms", () => {
+      cy.get('[href="/roomlist"] > .btn__content').click();
+      cy.url().should('include', '/roomlist');
+      cy.get("h2").should('contain', 'Room List');
+      cy.get('h2 > .btn > .btn__content').click();
+      cy.url().should('include', '/add-room');
+      cy.get("h2").should('contain', 'Add Room');
+      cy.get('#room_name').type('Experienced Drivers');
+      cy.get('form > .btn').click();
+      cy.visit("/");
+    });
+    describe("", () => {
+      it("", () => {
+
+      });
+    });
+  });
+
+});
+
