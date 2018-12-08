7fff9e7630f4659e6fb7f8844417d642dbe7bd7b
William Wall <william@williamwall.ie>
2018-12-08 14:22:56 +0000

    test to join a specific chat room with a nickname of choice

--- cypress/integration/5_chat_room.js
+++ cypress/integration/5_chat_room.js
@@ -23,7 +23,23 @@ describe("Chat Rooms", () => {
     });
   });
 
-
+  describe("Join a Chat Room", () => {
+    it("Should enter a Chat Room using a Nickname", () => {
+      cy.get('[href="/roomlist"] > .btn__content').click();
+      cy.url().should('include', '/roomlist');
+      cy.get("h2").should('contain', 'Room List');
+      cy.get(':nth-child(3) > .text-black').should('contain', 'Experienced Drivers');
+      cy.get(':nth-child(3) > [aria-colindex="3"] > .btn').click();
+      cy.get('.toolbar__items > [href="/roomlist"] > .btn__content').click();
+      cy.get(':nth-child(3) > [aria-colindex="3"] > .btn').click();
+      cy.get("h2").should('contain', 'Join Room');
+      cy.get('#fieldsetHorizontal__BV_label_').should('contain', 'Enter Nickname');
+      cy.get('#nickname').type('Mr.Wall');
+      cy.get('form > .btn').click();
+      cy.get("h2").should('contain', 'Chat Room');
+      cy.visit("/");
+    });
+  });
 
 
 });
